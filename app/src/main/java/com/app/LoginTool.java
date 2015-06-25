44aa6e6e66ad1c594664bce1bdc8b8390f0c39b5
Martin Tomik <mtomik@live.com>
2015-06-25 15:18:22 +0200

    Pridanie classy LoginTool

---
+++ app/src/main/java/com/app/LoginTool.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+
+public class LoginTool {
+	private WebDriver driver;
+	
+	public LoginTool(WebDriver driver){
+		this.driver = driver;
+	}
+
+	public void Log(){
+		    driver.findElement(By.id("_58_login")).sendKeys("admin");
+	        driver.findElement(By.id("_58_password")).sendKeys("test");
+	        driver.findElement(By.className("aui-button-input-submit")).click();
+	}
+	public void SignOut(){
+		driver.findElement(By.className("sign-out")).click();
+	}
+}
