cdaf55dce98b9d0a13430e9e1174f53b8871a3e2
Martin Tomik <mtomik@live.com>
2015-06-25 15:27:38 +0200

    Pridanie CR classy

--- app/src/main/java/com/app/App.java
+++ app/src/main/java/com/app/App.java
@@ -1,5 +1,11 @@
 package com.app;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.app.ChangeRequest;
+import com.app.LoginTool;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	 WebDriver driver = new FirefoxDriver();
+	        driver.get("https://testportal:7444/web/guest/home");
+	        
+	        
+	        
+	     //   if(driver.findElement(By.id("_58_login"). != null)
+	        
+	        //String elementval = driver.findElement(By.id("_58_login"));
+	        		
+	        //Log In
+	        LoginTool tool = new LoginTool(driver);
+	        tool.Log();
+	        
+	        
+	        ChangeRequest CR = new ChangeRequest(driver);
+	        CR.CreateCR();
+	        
+	        
+	        //Sign Out
+	        //tool.SignOut();
+
+	     /*   Worker worker = new Worker();
+	       driver.s*/
     }
 }
---
+++ app/src/main/java/com/app/ChangeRequest.java
@@ -0,0 +1,25 @@
+package com.app;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ChangeRequest {
+
+	private WebDriver driver;
+	
+	public ChangeRequest(WebDriver driver){
+		this.driver = driver;
+	}
+	public void CreateCR(){
+		//WebElement leftPanel = driver.findElement(By.className("portlet-body"));
+		
+		driver.findElement(By.cssSelector("a[href*='/group/wizardportal/change-request']")).click();
+		driver.findElement(By.cssSelector("div[title*='Create Change Request']")).click();
+		
+	}
+	
+	
+}
